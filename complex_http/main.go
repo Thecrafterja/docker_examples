951d836601d5fbc4d64852183d553f46014e0c23
Jannis Voß <info@jannisvoss.de>
2024-11-08 14:23:18 +0000

    Added http example

---
+++ complex_http/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	println("Starting webserver")
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World through the Web from Go!"))
+	})
+
+	http.ListenAndServe(":10101", router)
+}
