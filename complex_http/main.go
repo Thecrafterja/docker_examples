ce1327d97d5e7d9a4f00e8cc0c4caa1a8a8c43bc
Jannis Voß <info@jannisvoss.de>
2024-11-08 14:36:51 +0000

    Added comment to Dockerfile

--- complex_http/main.go
+++ complex_http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -15,6 +16,10 @@ func main() {
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World through the Web from Go!"))
+	})
+
+	router.Get("/time", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Aktueller Zeitpunkt: " + time.Now().Local().Format("01.02.2006 15:04:05")))
 	})
 
 	http.ListenAndServe(":10101", router)
